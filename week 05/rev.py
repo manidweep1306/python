e8b0613cd46684551d37598af1b16c3cdd6edef5
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2024-09-23 15:43:56 +0530

    Create rev.py

---
+++ week 05/rev.py
@@ -0,0 +1,15 @@
+l = []
+n = int(input("enter no of ele:"))
+for i in range(n):
+    e =int(input())
+    l.append(e)
+l.reverse()    
+print(l)
+    #OR
+l = []
+n = int(input("enter no of ele:"))
+for i in range(n):
+    e =int(input())
+    l.append(e)
+print(l[::-1])
+  #OR
