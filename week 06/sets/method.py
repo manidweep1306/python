13a51a1b722f0f2354bdf590fffc84a596a8ce01
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2024-09-30 15:42:58 +0530

    Create method.py

---
+++ week 06/sets/method.py
@@ -0,0 +1,27 @@
+t1 = {0,1,2,3,3,5,6,77,7,7}
+t2 = {4,5,6,7}
+t1.add('python')
+print(t1)
+
+t2.clear()
+print(t2)
+
+t1.discard(7)
+print(t1)
+
+t1.remove(3)
+print(t1)
+
+t1.pop()
+print(t1)
+
+t2 = t1.copy()
+print("t2=',t2)
+
+print(a.difference(b))
+
+print(a.symmetric_difference(b))
+
+print(a.intersection(b))
+
+print(a.union(b))
