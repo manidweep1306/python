3a6164b250b6e29620b4046405e0a4b13a25e304
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2024-10-21 16:18:16 +0530

    Create 2nd-largest.py

---
+++ week 07/2nd-largest.py
@@ -0,0 +1,9 @@
+def max(l):
+    l.sort()
+    print("the 2nd largest element in ",l,"is",l[-2])
+n = int(input('Enter the no. of elements: '))
+l = []
+print('Enter the content in 1st list: ')
+for i in range(n):
+    l.append(input())
+max(l)
