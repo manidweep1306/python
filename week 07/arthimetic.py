d15ac0ce81f6b6f12519d965dcd8d12b1de19866
manidweep1306 <166938022+manidweep1306@users.noreply.github.com>
2024-10-21 15:20:02 +0530

    Create arthimetic.py

---
+++ week 07/arthimetic.py
@@ -0,0 +1,8 @@
+def Arthimetic():
+	print("addition:",a+b)
+	print("subtraction:",a-b)
+	print("multiplication:",a*b)
+	print("division:",a/b)
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+Arthimetic()
